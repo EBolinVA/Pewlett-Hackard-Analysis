6dc0e69f2854376aeefed23ead003c222c6c6408
EBolinVA <elizabeth.bolin@gmail.com>
2022-12-15 09:32:21 -0500

    update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 --Creating tables for PH-EmployeeDB
-CREATE TABLE departments (
+CREATE TABLE departments(
 	dept_no VARCHAR(4) NOT NULL,
-	dept_name VARCHAR(40) NOT NULL, 
-	PRIMARY KEY (dept_no),
+	dept_name VARCHAR(40) NOT NULL,
+	PRIMARY KEY (dept_no), 
 	UNIQUE (dept_name)
 );
 
@@ -47,11 +47,11 @@ CREATE TABLE dept_emp (
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
+	title VARCHAR(50) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
-SELECT * FROM departments;
+SELECT * FROM titles;
